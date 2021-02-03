77fd21075d8db2e9f7ff409577c41b956c270bc5
Anita-Heiblom <61972759+Anita-Heiblom@users.noreply.github.com>
2021-02-03 08:31:45 +0100

    Added component with target text to add interaction

--- src/prefabs/stylesheet.js
+++ src/prefabs/stylesheet.js
@@ -1,20 +1,19 @@
 (() => ({
-  name: 'customStyleSheet',
-  icon: 'HtmlIcon',
-  category: 'STYLING',
-  structure: [
-    {
-      name: 'customStyleSheet',
-      options: [
-        {
-          type: 'TEXT',
-          label: 'Stylesheet URL',
-          key: 'styleSheetUrl',
-          value:
-            '//assets.bettyblocks.com/0b97639d88cc40b8b26761748f9a1b31_assets/files/custom_styles3.css',
-        },
-      ],
-      descendants: [],
-    },
-  ],
+	name: "customStyleSheet",
+	icon: "HtmlIcon",
+	category: "STYLING",
+	structure: [
+		{
+			name: "customStyleSheet",
+			options: [
+				{
+					type: "TEXT",
+					label: "Stylesheet URL",
+					key: "styleSheetUrl",
+					value: "",
+				},
+			],
+			descendants: [],
+		},
+	],
 }))();
