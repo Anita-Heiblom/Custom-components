640a4cafbfb4e227d8505ba1cad8851d795c9b3e
Anita-Heiblom <61972759+Anita-Heiblom@users.noreply.github.com>
2021-02-26 15:30:12 +0100

    Added zoom image option to media component

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  },
-  output: {
-    library: 'MaterialUI',
-    libraryExport: 'default',
-    libraryTarget: 'umd',
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	mode: 'production',
+	entry: './src/index.js',
+	externals: {
+		react: 'React',
+		'react-dom': 'ReactDOM',
+	},
+	output: {
+		library: 'MaterialUI',
+		libraryExport: 'default',
+		libraryTarget: 'umd',
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
 };
