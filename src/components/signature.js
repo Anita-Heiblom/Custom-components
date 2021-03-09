0ea47bbe7371f9ed9f8fa4f372eda76f6b4b33f3
Anita-Heiblom <61972759+Anita-Heiblom@users.noreply.github.com>
2021-03-09 14:34:25 +0100

    Added validation

--- src/components/signature.js
+++ src/components/signature.js
@@ -4,45 +4,24 @@
 	allowedTypes: [],
 	orientation: 'HORIZONTAL',
 	jsx: (() => {
-		const { env, getCustomModelAttribute, useFileUpload, useText } = B;
+		const { env, getCustomModelAttribute, useText } = B;
+		const SignaturePad = window.SignaturePad;
 		const {
 			FormControl,
 			FormControlLabel,
 			FormHelperText,
-			Button,
-			Typography,
-			IconButton,
 		} = window.MaterialUI.Core;
-		const { Icons } = window.MaterialUI;
-		const { Delete, CloudUpload } = Icons;
 		const {
-			hideDefaultError,
-			disabled,
 			helperText,
 			fullWidth,
-			size,
-			accept,
+			error,
+			validationValueMissing,
 			margin,
-			variant,
-			icon,
-			iconPosition,
-			buttonText,
-			multiple,
 			hideLabel,
 			customModelAttribute: customModelAttributeObj,
 			nameAttribute,
-			type,
-			showImagePreview,
 		} = options;
-
 		const isDev = env === 'dev';
-		const inputRef = React.createRef();
-		const [uploads, setUploads] = useState({
-			files: [],
-			data: [],
-			failureMessage: [],
-		});
-		const helper = useText(helperText);
 		const { id: customModelAttributeId, label = [] } = customModelAttributeObj;
 		const labelText = useText(label);
 		const customModelAttribute = getCustomModelAttribute(
@@ -52,373 +31,115 @@
 			customModelAttribute || {};
 		const nameAttributeValue = useText(nameAttribute);
 		const requiredText = required ? '*' : '';
-		const [uploadedFileArray, setUploadedFileArray] = useState([]);
+		const [errorState, setErrorState] = useState(error);
+		const [value, setValue] = useState('');
+		const signaturePadRef = useRef(null);
+		const [helper, setHelper] = useState(useText(helperText));
 
-		const formatBytes = (bytes) => {
-			if (bytes === 0) return '0 Bytes';
-			const k = 1024;
-			const sizes = ['Bytes', 'KB', 'MB'];
-			const i = Math.floor(Math.log(bytes) / Math.log(k));
-			return `${parseFloat(bytes / k ** i).toFixed()} ${sizes[i]}`;
-		};
+		B.defineFunction('ClearSignature', () => signaturePadRef.current.clear());
 
-		const handleChange = (e) => {
-			setUploadedFileArray((prev) => [...prev, ...e.target.files]);
-			setUploads({
-				...uploads,
-				files: e.target.files,
-			});
-		};
+		const handleChange = (event) => {
+			const { target } = event;
+			let { validity: validation } = target;
+			handleValidation(validation);
 
-		const clearFiles = (e) => {
-			if (e && e.preventDefault) e.preventDefault();
-			setUploads({
-				files: [],
-				data: [],
-				failureMessage: [],
-			});
-		};
-
-		const { files, data, failureMessage } = uploads;
-
-		const acceptedValue = useText(accept) || 'image/*';
-		const acceptList = acceptedValue.split(',').map((item) => item.trim());
-		const helperValue =
-			!hideDefaultError && failureMessage.length > 0 ? failureMessage : helper;
-
-		const [uploadFile, { loading } = {}] = useFileUpload({
-			options: {
-				variables: {
-					fileList: Array.from(files),
-					mimeType: acceptList,
-				},
-				onError: (errorData) => {
-					B.triggerEvent('onError', errorData);
-					setUploads({
-						...uploads,
-						failureMessage: [errorData.message],
-					});
-				},
-				onCompleted: (uploadData) => {
-					const { uploadFiles } = uploadData;
-
-					const [succeededData, failedData] = uploadFiles.reduce(
-						(result, d) => {
-							result[d.url.startsWith('http') ? 0 : 1].push(d);
-							return result;
-						},
-						[[], []]
-					);
-
-					const formattedFailedData = failedData.map((d) => (
-						<div>{`File: ${d.name} failed with error: ${d.url}`}</div>
-					));
-
-					if (succeededData.length > 0) {
-						B.triggerEvent('onSuccess', succeededData);
-					} else {
-						B.triggerEvent('onNoResults');
-					}
-					if (failedData.length > 0) {
-						B.triggerEvent('onError', formattedFailedData);
-					}
-					setUploads({
-						...uploads,
-						data: multiple ? data.concat(succeededData) : succeededData,
-						failureMessage: formattedFailedData,
-					});
-				},
-			},
-		});
-
-		const removeFileFromList = (fileUrl) => {
-			const newList = data.filter((d) => d.url !== fileUrl);
-			setUploads({
-				...uploads,
-				data: newList,
-			});
-		};
-
-		const UploadComponent = (
-			<div
-				className={[classes.control, fullWidth ? classes.fullwidth : ''].join(
-					' '
-				)}
-			>
-				<input
-					accept={acceptedValue}
-					className={classes.input}
-					multiple={multiple}
-					type='file'
-					onChange={handleChange}
-					ref={inputRef}
-				/>
-				<Button
-					size={size}
-					variant={variant}
-					classes={{
-						root: classes.button,
-						contained: classes.contained,
-						outlined: classes.outlined,
-					}}
-					component='span'
-					disabled={disabled}
-					startIcon={
-						variant !== 'icon' &&
-						icon !== 'None' &&
-						iconPosition === 'start' &&
-						React.createElement(Icons[icon])
-					}
-					endIcon={
-						variant !== 'icon' &&
-						icon !== 'None' &&
-						iconPosition === 'end' &&
-						React.createElement(Icons[icon])
-					}
-				>
-					{variant === 'icon'
-						? React.createElement(Icons[icon === 'None' ? 'Error' : icon], {
-								fontSize: size,
-						  })
-						: useText(buttonText)}
-				</Button>
-				{data.length > 0 && (
-					<input
-						type='hidden'
-						name={nameAttributeValue || customModelAttributeName}
-						value={data.map((d) => d.url).join(',')}
-					/>
-				)}
-			</div>
-		);
-
-		const Hr = () => <hr className={classes.hr} />;
-
-		const DeleteButton = ({ file }) => (
-			<div className={classes.deleteButtonWrapper}>
-				<IconButton
-					size='small'
-					className={classes.remove}
-					onClick={() => {
-						if (file) {
-							removeFileFromList(file.url);
-							if (!multiple) {
-								B.triggerEvent('onFileRemove');
-							}
-						}
-					}}
-				>
-					<Delete className={classes.deleteIcon} fontSize='small' />
-				</IconButton>
-			</div>
-		);
-		const FileDetails = ({ file, fileType, fileSize }) => (
-			<div className={classes.fileDetails}>
-				<Typography variant='body1' noWrap className={classes.span}>
-					{file ? file.name : 'File name'}
-				</Typography>
-				<div className={classes.fileDetailList}>
-					<p className={classes.fileDetail}>
-						{isDev ? 'Size' : formatBytes(fileSize)}
-					</p>
-					<div className={classes.divider} />
-					<p className={classes.fileDetail}>
-						{isDev ? 'Type' : fileType.replace('image/', '.')}
-					</p>
-				</div>
-			</div>
-		);
-
-		const DevUploadedFile = () => {
-			switch (type) {
-				case 'grid':
-					return (
-						<div className={classes.gridView}>
-							<div className={classes.gridItem}>
-								{showImagePreview && <div className={classes.gridDevImage} />}
-								<div className={classes.gridItemDetails}>
-									<FileDetails />
-									<DeleteButton />
-								</div>
-							</div>
-						</div>
-					);
-				case 'list':
-				default:
-					return (
-						<>
-							<Hr />
-							<div className={classes.listView}>
-								<div className={classes.fileDetailList}>
-									{showImagePreview && <div className={classes.devImage} />}
-									<FileDetails />
-								</div>
-								<DeleteButton />
-							</div>
-						</>
-					);
+			if (!signaturePadRef.current.isEmpty()) {
+				const image = signaturePadRef.current.toDataURL();
+				const splittedImage = image.split(',');
+				setValue(splittedImage[splittedImage.length - 1]);
 			}
 		};
 
-		const UploadedFile = ({ file }) => {
-			const uploadedFile = uploadedFileArray.find(
-				(item) => item.name === file.name
-			);
-
-			if (!multiple) {
-				B.triggerEvent('onFileUpload', file.url);
-			}
-
-			switch (type) {
-				case 'grid':
-					return (
-						<div className={classes.gridView}>
-							<div className={classes.gridItem}>
-								{showImagePreview && (
-									<div
-										style={{
-											backgroundImage: `url("${file.url}")`,
-										}}
-										className={classes.gridImage}
-									/>
-								)}
-								<div className={classes.gridItemDetails}>
-									<FileDetails
-										file={file}
-										fileType={uploadedFile.type}
-										fileSize={uploadedFile.size}
-									/>
-									<DeleteButton file={file} />
-								</div>
-							</div>
-						</div>
-					);
-				case 'list':
-				default:
-					return (
-						<>
-							<Hr />
-							<div className={classes.listView}>
-								<div className={classes.fileDetailList}>
-									{showImagePreview && (
-										<div
-											style={{
-												backgroundImage: `url("${file.url}")`,
-											}}
-											className={classes.image}
-										/>
-									)}
-									<FileDetails
-										file={file}
-										fileType={uploadedFile.type}
-										fileSize={uploadedFile.size}
-									/>
-								</div>
-								<DeleteButton file={file} />
-							</div>
-						</>
-					);
-			}
+		const handleValidation = (validation) => {
+			setErrorState(!validation.valid);
+			const message = useText(validationValueMissing) || useText(helperText);
+			setHelper(message);
 		};
 
-		const UploadingFile = () => {
-			switch (type) {
-				case 'grid':
-					return (
-						<div className={classes.gridView}>
-							<div className={classes.gridItem}>
-								{showImagePreview && (
-									<div className={classes.gridUploadingImage}>
-										<CloudUpload />
-									</div>
-								)}
-								<div className={classes.gridItemDetails}>
-									<span>Uploading</span>
-								</div>
-							</div>
-						</div>
-					);
-				case 'list':
-				default:
-					return (
-						<>
-							<Hr />
-							<div className={classes.listView}>
-								{showImagePreview && (
-									<div className={classes.uploadingImage}>
-										<CloudUpload />
-									</div>
-								)}
-								<div className={classes.fileDetails}>
-									<span>Uploading</span>
-								</div>
-							</div>
-						</>
-					);
-			}
+		const invalidHandler = (event) => {
+			event.preventDefault();
+			const {
+				target: { validity },
+			} = event;
+			handleValidation(validity);
 		};
 
 		const Control = () => (
 			<FormControl
 				fullWidth={fullWidth}
 				required={required}
-				error={!hideDefaultError && failureMessage.length > 0}
-				disabled={disabled}
 				margin={margin}
+				error={errorState}
 			>
 				<FormControlLabel
-					control={UploadComponent}
+					control={
+						<div
+							className={[
+								classes.signaturePad,
+								fullWidth ? classes.fullwidth : '',
+							].join(' ')}
+							onMouseUp={handleChange}
+						>
+							<SignaturePad ref={signaturePadRef} onMouseUp={handleChange} />
+							<input
+								hidden
+								name={nameAttributeValue || customModelAttributeName}
+								value={value}
+								required={required}
+								onInvalid={invalidHandler}
+							/>
+						</div>
+					}
 					label={hideLabel ? '' : `${labelText}${requiredText}`}
 					labelPlacement='top'
 					classes={{
 						root: classes.label,
 					}}
 				/>
-				<FormHelperText classes={{ root: classes.helper }}>
-					{helperValue}
-				</FormHelperText>
-				<div className={classes.messageContainer}>
-					{data &&
-						data.length > 0 &&
-						data.map((file) => <UploadedFile file={file} />)}
-					{loading && <UploadingFile />}
-				</div>
+				{helper && (
+					<FormHelperText classes={{ root: classes.helper }}>
+						{helper}
+					</FormHelperText>
+				)}
 			</FormControl>
 		);
 
-		useEffect(() => {
-			if (loading) {
-				B.triggerEvent('onLoad');
-			}
-		}, [loading]);
-
-		useEffect(() => {
-			if (files.length > 0) {
-				uploadFile();
-			}
-		}, [files]);
-
-		B.defineFunction('clearFileUpload', (e) => clearFiles(e));
-
 		return isDev ? (
-			<div>
-				<div className={classes.root}>
-					<Control />
-				</div>
-				<DevUploadedFile />
+			<div id='signature' className={classes.root}>
+				{Control()}
 			</div>
 		) : (
-			<Control />
+			<div id='signature'>{Control()}</div>
 		);
 	})(),
 	styles: (B) => (t) => {
 		const { color: colorFunc, env, Styling } = B;
 		const style = new Styling(t);
+
 		const getOpacColor = (col, val) => colorFunc.alpha(col, val);
+
 		return {
 			root: {
-				display: ({ options: { fullWidth } }) =>
-					fullWidth ? 'block' : 'inline-block',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				height: '100%',
+				width: '100%',
+				fontSize: '0.75rem',
+				color: '#262A3A',
+				boxSizing: 'border-box',
+				borderWidth: '0.0625rem',
+				borderColor: '#AFB5C8',
+				borderStyle: 'dashed',
+				backgroundColor: '#F0F1F5',
+			},
+			signaturePad: {
+				border: '1px solid',
+				borderRadius: '4px',
+				borderColor: ({ options: { borderColor } }) => [
+					style.getColor(borderColor),
+					'!important',
+				],
 			},
 			label: {
 				marginLeft: '0!important',
@@ -433,10 +154,6 @@
 						style.getColor(errorColor),
 						'!important',
 					],
-				},
-				'&.Mui-disabled': {
-					pointerEvents: 'none',
-					opacity: '0.7',
 				},
 			},
 			helper: {
@@ -467,28 +184,6 @@
 				textAlign: 'start',
 				marginBottom: '0.1875rem!important',
 				marginRight: '1rem!important',
-			},
-			button: {
-				color: ({ options: { variant, buttonTextColor, background } }) => [
-					style.getColor(variant === 'icon' ? background : buttonTextColor),
-					'!important',
-				],
-				'&.Mui-disabled': {
-					pointerEvents: 'none',
-					opacity: '0.7',
-				},
-			},
-			contained: {
-				backgroundColor: ({ options: { background } }) => [
-					style.getColor(background),
-					'!important',
-				],
-			},
-			outlined: {
-				borderColor: ({ options: { background } }) => [
-					style.getColor(background),
-					'!important',
-				],
 			},
 			messageContainer: {
 				flexWrap: 'wrap',
@@ -529,102 +224,8 @@
 				margin: '1rem',
 				justifyContent: 'space-between',
 			},
-			devImage: {
-				display: 'flex',
-				overflow: 'hidden',
-				margin: '1rem',
-				borderStyle: 'dashed',
-				borderColor: '#AFB5C8',
-				borderRadius: '0.3125rem',
-				borderWidth: '0.0625rem',
-				border: '0.125rem dashed black',
-				backgroundColor: '#F0F1F5',
-				width: ({ options: { imagePreviewWidth } }) => imagePreviewWidth,
-				height: ({ options: { imagePreviewHeight } }) => imagePreviewHeight,
-			},
-			gridDevImage: {
-				extend: 'devImage',
-
-				margin: 0,
-				borderRadius: '0.3125rem 0.3125rem 0 0',
-				width: ({ options: { imagePreviewWidth } }) => imagePreviewWidth,
-				height: ({ options: { imagePreviewHeight } }) => imagePreviewHeight,
-			},
 			deleteIcon: {
 				color: `${t.colors.light}!important`,
-			},
-			uploadingImage: {
-				border: 'none',
-				color: 'white',
-				display: 'flex',
-				overflow: 'hidden',
-				margin: '1rem',
-				alignItems: 'center',
-				borderRadius: '0.3125rem',
-				justifyContent: 'center',
-				backgroundColor: t.colors.warning,
-				width: ({ options: { imagePreviewWidth } }) => imagePreviewWidth,
-				height: ({ options: { imagePreviewHeight } }) => imagePreviewHeight,
-			},
-			gridUploadingImage: {
-				extend: 'uploadingImage',
-
-				margin: 0,
-				borderRadius: '0.3125rem 0.3125rem 0 0',
-				width: ({ options: { imagePreviewWidth } }) => imagePreviewWidth,
-				height: ({ options: { imagePreviewHeight } }) => imagePreviewHeight,
-			},
-			image: {
-				overflow: 'hidden',
-				margin: '1rem',
-				borderRadius: '0.3125rem',
-				backgroundSize: 'cover',
-				width: ({ options: { imagePreviewWidth } }) => imagePreviewWidth,
-				height: ({ options: { imagePreviewHeight } }) => imagePreviewHeight,
-			},
-			gridImage: {
-				margin: 0,
-				overflow: 'hidden',
-				backgroundSize: 'cover',
-				borderRadius: '0.3125rem 0.3125rem 0 0',
-				width: ({ options: { imagePreviewWidth } }) => imagePreviewWidth,
-				height: ({ options: { imagePreviewHeight } }) => imagePreviewHeight,
-			},
-			fileDetails: {
-				flexGrow: 1,
-				maxWidth: ({
-					options: { type, imagePreviewWidth, showImagePreview },
-				}) =>
-					showImagePreview && type === 'grid'
-						? `calc(${imagePreviewWidth} - 60px)`
-						: 'auto',
-				display: 'flex',
-				flexDirection: 'column',
-			},
-			fileDetail: {
-				margin: 0,
-				color: t.colors.medium,
-			},
-			fileDetailList: {
-				display: 'flex',
-				alignItems: 'center',
-			},
-			divider: {
-				width: '0.1875rem',
-				height: '0.1875rem',
-				borderRadius: '50%',
-				marginLeft: '1rem',
-				backgroundColor: t.colors.light,
-				marginRight: '1rem',
-			},
-			hr: {
-				height: 1,
-				margin: 0,
-				border: 'none',
-				backgroundColor: t.colors.light,
-			},
-			deleteButtonWrapper: {
-				margin: ({ options: { type } }) => (type === 'grid' ? 0 : '1rem'),
 			},
 			remove: {
 				height: '1.875rem',
