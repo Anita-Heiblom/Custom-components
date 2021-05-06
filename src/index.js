e259b5f13c6fe0809f2759b58f0249217b963bbe
Anita-Heiblom <61972759+Anita-Heiblom@users.noreply.github.com>
2021-05-06 08:31:40 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,17 +5,8 @@ import * as Styles from '@material-ui/styles';
 import DateFnsUtils from '@date-io/date-fns';
 import enLocale from 'date-fns/locale/en-US';
 import nlLocale from 'date-fns/locale/nl';
-import InnerImageZoom from 'react-inner-image-zoom';
-import css from 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
-import FullCalendar from '@fullcalendar/react';
 
 import Icons from './icons';
-
-window.css = css;
-
-window.InnerImageZoom = InnerImageZoom;
-
-window.FullCalendar = FullCalendar;
 
 export default {
 	Core,
